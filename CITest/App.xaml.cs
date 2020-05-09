0c23a92d7088ecdc1c4f713649a41f839db9bdcf
erandika86 <30391205+erandika86@users.noreply.github.com>
2020-05-09 11:23:49 +0530

    SciChart Added

--- CITest/App.xaml.cs
+++ CITest/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using SciChart.Charting.Visuals;
 
 namespace CITest
 {
@@ -13,5 +14,10 @@ namespace CITest
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            // Set this code once in App.xaml.cs or application startup
+            SciChartSurface.SetRuntimeLicenseKey("FDqs1EPN/0q8Sb/GrmmcGYgt2WfywzkAX8VjrtXOAplNrlMwNz9a35Vd2NKIH3msLLUx+ROS6LLSt8Dk9coSQq7NbGzPzRO8D/CXBPj6N+VtUonxE6X3rjlyNq/TP+jslA9QPCxKXu0jzpmfnzc4yMBabWRVZ/7e5Febk2tKmmTLGAp/RnaqvKi02rjssq+gNJZtbANOn7CF9m39y2ULWRnDa/UVo87bQPuuzHoDzZflxIbCwdrcFxRNOV7O3nU235QQI8MaPZsl6ZQmCakvV2WbDC00hIp04tYM9A5nV/zQITweL3sR7aiTrRdk1DXqO6mEfiFrTHnkKpLwpNZeETEfQDzpTtsk8GgOUxVH5bcFx2NXWjHN0rKUnf/lTLoYgIlMpq1sD/MEsVOgYLmgP44XkXWyB/3MG2bsoH61XPh5dIeOKYKpwD6IWmBi4/hm4i6G4o9nrxdKS+aOgsyXSKyTWSzqG3Ez9EwjOGYZb+0I/g3TUuPZ90hIeIJinBX9XpLzu2LrQJOteAuBEBM3EBlx4BqWlK+qUcC+32simkSwRwc8AtKL4ps/Uy9TNHypk1XTlQEQlay2hg==");
+        }
     }
 }
